93f27112737f9ae21a043ac1bf9bb29a0ea45596
Thomas Pope <tpope0928@gmail.com>
2019-01-15 22:06:35 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -9,11 +9,11 @@ class Artist < ActiveRecord::Base
 
   def song_count
     #return the number of songs associated with the artist
-    self.song.size
+    self.songs.size
   end
 
   def genre_count
     #return the number of genres associated with the artist
-    self.genre.size
+    self.genres.size
   end
 end
