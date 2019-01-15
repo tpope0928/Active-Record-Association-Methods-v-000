47081b2ceb70a72cc22d8b57e7412013df2408d8
Thomas Pope <tpope0928@gmail.com>
2019-01-15 22:10:51 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -9,11 +9,11 @@ class Genre < ActiveRecord::Base
 
   def artist_count
     # return the number of artists associated with the genre
-    self.artist.size
+    self.artists.size
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artists.collect{|artists|artist.name}
+    self.artists.collect {|artist| artist.name}
   end
 end
